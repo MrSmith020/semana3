d30df22fc5d84e3db9c49f3594f61de67c107152
Rasputin <luisaguayo020@gmail.com>
2021-06-05 11:05:10 -0500

    probando ejercicio 7 y github

---
+++ ejecicio7.js
@@ -0,0 +1,13 @@
+/*Escriba un bucle que solicite un número mayor que 100. 
+Si el visitante ingresa otro número, pídale que ingrese nuevamente.
+El bucle debe solicitar un número hasta que el visitante ingrese un número mayor que 100 o cancele 
+la entrada / ingrese una línea vacía. 
+Aquí podemos asumir que el visitante solo ingresa números.
+No es necesario implementar un manejo especial para una entrada no numérica en esta tarea.*/
+
+numero=prompt("ingrese numero");
+if (numero>=100){
+
+}else{
+    document.write("valor ingresado es "+ numero);
+}
